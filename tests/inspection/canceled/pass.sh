c7378c2e645fa9119392da3dfcac250ce0c1998c
Abrar Quazi <abrar.quazi@sifive.com>
2026-01-29 08:06:06 -0800

    Fix flakey inspection test by increasing timeout

--- tests/inspection/canceled/pass.sh
+++ tests/inspection/canceled/pass.sh
@@ -7,6 +7,10 @@ WAKE="${1:+$1/wake}"
 rm -f wake.db
 
 # Use || true to ignore the expected non-0 return from timeout
-timeout 1 ${WAKE} test || true
+timeout 3 ${WAKE} test || true
+
+# Give the database a moment to ensure WAL is flushed
+# This is important because wake might be killed mid-transaction
+sleep 0.2
 
 ${WAKE} --canceled --simple
