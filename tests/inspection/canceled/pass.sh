32f62b795c67a5e99509242317ceb605c9a7f797
Abrar Quazi <abrar.quazi@sifive.com>
2026-01-29 09:08:18 -0800

    remove unecessary sleep

--- tests/inspection/canceled/pass.sh
+++ tests/inspection/canceled/pass.sh
@@ -9,8 +9,5 @@ rm -f wake.db
 # Use || true to ignore the expected non-0 return from timeout
 timeout 3 ${WAKE} test || true
 
-# Give the database a moment to ensure WAL is flushed
-# This is important because wake might be killed mid-transaction
-sleep 0.2
 
 ${WAKE} --canceled --simple
