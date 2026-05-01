89a867b60fd2d6f6c00cb24bc20ee18d17f235e2
Will Dietz <will.dietz@sifive.com>
2026-05-01 08:24:20 -0700

    Revert "mtime-preservation: Explicitly serialize wake invocations."
    Not neeed for multi-wake, was temporary workaround.
    
    cc #1830.
    
    This reverts commit d9b0c58aca8dc1459dfed3ef0e963208a708ea1e.

--- tests/runtime/mtime-preservation/pass.sh
+++ tests/runtime/mtime-preservation/pass.sh
@@ -12,18 +12,23 @@ export WAKE_CAS=1
 
 rm -rf .build .fuse wake.db* wake.log output.txt result-a.txt result-b.txt
 
-echo "Fresh runs:"
+echo "Fresh concurrent (if supported) runs:"
 
-"${WAKE}" -q --no-tty -x "consumerA Unit"
-"${WAKE}" -q --no-tty -x "consumerB Unit"
+"${WAKE}" -q --no-tty -x "consumerA Unit" &
+"${WAKE}" -q --no-tty -x "consumerB Unit" &
 
+wait
+
+# (output.txt can have either value)
 tail result-a.txt result-b.txt
 
 echo
 echo "Reuse:"
 
-"${WAKE}" -q --no-tty -x "consumerA Unit"
-"${WAKE}" -q --no-tty -x "consumerB Unit"
+"${WAKE}" -q --no-tty -x "consumerA Unit" &
+"${WAKE}" -q --no-tty -x "consumerB Unit" &
+
+wait
 
 # (output.txt can have either value)
 tail result-a.txt result-b.txt
