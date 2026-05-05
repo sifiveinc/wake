df4891ff77d95570abc26a2d1658ebf9e517b937
Will Dietz <will.dietz@sifive.com>
2026-05-05 14:30:46 -0700

    tests: update hardlink test to check mode, test WAKE_CAS=1.

--- tests/wakebox/hardlink/pass.sh
+++ tests/wakebox/hardlink/pass.sh
@@ -5,6 +5,4 @@ export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin
 
 trap 'rm -f link_file_src.txt link_file_dst.txt' EXIT
 
-# Expect an error like "permission denied".
-# Not presently checked for explicitly due to variations.
 ${1}/wakebox -p input.json
